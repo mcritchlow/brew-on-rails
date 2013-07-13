de2df8b7a13046caf844740f4197b89edf2dbb7c
Matt Critchlow <mcritchlow@ucsd.edu>
2013-07-12 18:51:08 -0700

    here we go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrewOnRails::Application.config.secret_token = 'c1fbfae87ca40772a94da146e6a67d9fc2ed50f664513ad9d0f9d2c4faa2a82df28c2f5684d4a107a2408e324c79a20467bb21e0bea5e1b7fc1d4a60f47d02cb'
